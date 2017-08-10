3cb0bfe254f21a6393327b1153a41d6536369ced
Manuel Ilg <manuelilg@bluemail.ch>
2017-08-10 15:29:47 +0200

    command input to controller

--- motor_controller/motor_controller.cpp
+++ motor_controller/motor_controller.cpp
@@ -5,11 +5,13 @@
 
 class MotorControllerNode {
  public:
-  MotorControllerNode(): phi_tm1(0.0), error_tm1(0.0), integrator(0.0) {
+  MotorControllerNode(): phi_tm1(0.0), error_tm1(0.0), integrator(0.0), command(0.0) {
     ros::NodeHandle nh;
 		sub_ = nh.subscribe("motor/joint_states", 1, &MotorControllerNode::update, this);
 //		pub_ = nh.advertise<std_msgs::Float64>("motor_correction_variable", 100);
 		pub_ = nh.advertise<sensor_msgs::JointState>("/motor/motor_effort", 1);
+
+		sub2_ = nh.subscribe("motor/command", 1, &MotorControllerNode::setCommand, this);
   }
 
   virtual ~MotorControllerNode() {
@@ -21,9 +23,9 @@ class MotorControllerNode {
   }
 
 	void update(const sensor_msgs::JointStateConstPtr& msg_in) {
-    double phi_t = msg_in->position[0];
+		double phi_t = msg_in->position[0];
 		double omega_t = (phi_t - phi_tm1)/1e-3;
-		double error_t = 1 - omega_t;
+		double error_t = command - omega_t;
 		double p_term = error_t * 16.5;
 		integrator += (error_t + error_tm1)/2.0 * 1e-3;
 		double i_term = integrator * 32.5;
@@ -39,21 +41,27 @@ class MotorControllerNode {
 		msg_out.effort[0] = torque;
 		pub_.publish(msg_out);
 
-    ROS_INFO("Motor phi: %f", phi_t);
-    ROS_INFO("Motor omega: %f", omega_t);
-    ROS_INFO("Force to set: %f", torque);
+		ROS_INFO("Motor phi: %f", phi_t);
+		ROS_INFO("Motor omega: %f", omega_t);
+		ROS_INFO("Force to set: %f", torque);
 
 		phi_tm1 = phi_t;
 		error_tm1 = error_t;
   }
 
+	void setCommand(const std_msgs::Float64ConstPtr& msg) {
+		command = msg->data;
+	}
+
 
  private:
   ros::Subscriber sub_;
+  ros::Subscriber sub2_;
   ros::Publisher pub_;
-	double phi_tm1;
-	double integrator;
-	double error_tm1;
+  double command;
+  double phi_tm1;
+  double integrator;
+  double error_tm1;
 
 	static const double km = 0.0163;
 };
