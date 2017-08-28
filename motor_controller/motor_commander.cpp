24c7a218c41085714e657c5b899f96bc24d20666
Manuel Ilg <manuelilg@bluemail.ch>
2017-08-28 17:58:53 +0200

    motor w commander

---
+++ motor_controller/motor_commander.cpp
@@ -0,0 +1,58 @@
+#include <ros/ros.h>
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/JointState.h>
+
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "motor_controller");
+	//pub = nh.advertise<std_msgs::Float64>("motor_command", 100);
+	//sub = nh.subscribe("motor_position", 100, update);
+
+  ros::NodeHandle nh;
+	ros::Rate loop_rate(1000);
+
+	auto pub = nh.advertise<std_msgs::Float64>("rosNodeTalker/TestTopic1", 10);
+
+
+	int a = 50;
+	double t_a = 1.0;
+
+	double output_increment = a/1000.0;
+
+//	int counter = 0;
+	double max = t_a*50;
+
+
+	double output = 0.0;
+
+	bool accelerate = true;
+
+	while(ros::ok())
+	{
+		//ros::spinOnce;
+		std_msgs::Float64 msg;
+		msg.data = output;
+		pub.publish(msg);
+
+		if(accelerate) {
+			output += output_increment;
+			if(output>max) {
+				accelerate = !accelerate;
+			}
+		} else {
+			output -= output_increment;
+			if(output<0.0) {
+				accelerate = !accelerate;
+			}
+		}
+
+
+
+		loop_rate.sleep();
+	}
+
+//	ros::spin();
+
+	return 0;
+}
+
